b3a0dba3cd0427a6a395452f6464d0852d72e505
librucha <libor.ondrusek@gmail.com>
2015-01-06 21:09:04 +0100

    initial commit

---
+++ src/org/librucha/intellij/plugin/datagenerator/DataGeneratorAction.java
@@ -0,0 +1,14 @@
+package org.librucha.intellij.plugin.datagenerator;
+
+import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.actions.BaseCodeInsightAction;
+import org.jetbrains.annotations.NotNull;
+
+public class DataGeneratorAction extends BaseCodeInsightAction {
+
+	@NotNull
+	@Override
+	protected CodeInsightActionHandler getHandler() {
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+}
